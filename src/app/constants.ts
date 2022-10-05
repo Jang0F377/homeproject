96f08e0f5e04782787914640da37b9fb13c0c18a
Matt Garrett <mjgarrett7092@gmail.com>
2022-10-04 19:00:38 -0700

    constants

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -6,7 +6,7 @@ const nestHeaders = {
 export const myHeaders = new Headers(nestHeaders);
 
 // DO IP http://147.182.161.194:3000/
-export const EXPENSE_URL = "http://147.182.161.194:3000/expenses/";
+export const EXPENSE_URL = "https://147.182.161.194:3000/expenses/";
 export const PROJECT_URL = "http://147.182.161.194:3000/projects/";
 export const SARAH_URL = "http://147.182.161.194:3000/sarah/";
 export const MATT_URL = "http://147.182.161.194:3000/matt/";
