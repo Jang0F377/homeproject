5232a666964eca058430c8c248d795bc19c19bbf
Matt Garrett <mjgarrett7092@gmail.com>
2022-10-04 17:20:07 -0700

    ip change

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -5,9 +5,9 @@ const nestHeaders = {
 
 export const myHeaders = new Headers(nestHeaders);
 
-// Vultr IP http://66.42.108.135:3000/
-export const EXPENSE_URL = "http://localhost:3000/expenses/";
-export const PROJECT_URL = "http://localhost:3000/projects/";
-export const SARAH_URL = "http://localhost:3000/sarah/";
-export const MATT_URL = "http://localhost:3000/matt/";
-export const LOGIN_URL = "http://localhost:3000/login/";
+// DO IP http://147.182.161.194:3000/
+export const EXPENSE_URL = "http://147.182.161.194:3000/expenses/";
+export const PROJECT_URL = "http://147.182.161.194:3000/projects/";
+export const SARAH_URL = "http://147.182.161.194:3000/sarah/";
+export const MATT_URL = "http://147.182.161.194:3000/matt/";
+export const LOGIN_URL = "http://147.182.161.194:3000/login/";
